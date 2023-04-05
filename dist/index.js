641839e050b056f75999eba0fc4056806b5acff6
Jordan Wright <wright.jordan.n@outlook.com>
2023-04-04 19:20:47 -0500

    first commit

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,23 @@
-"use strict";
-console.log("Hello, create-node-package!");
+const lookup = new Map([
+    ["&", "&amp;"],
+    ['"', "&quot;"],
+    ["'", "&apos;"],
+    ["<", "&lt;"],
+    [">", "&gt;"],
+]);
+function replacer(ch) {
+    return lookup.get(ch);
+}
+export function html(strings, ...args) {
+    let transformed = "";
+    for (let i = 0; i < strings.length; i++) {
+        const arg = args[i];
+        if (arg) {
+            transformed += strings[i] + arg.replace(/[&"'<>]/g, replacer);
+        }
+        else {
+            transformed += strings[i];
+        }
+    }
+    return transformed;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello, create-node-package!");
+const lookup = new Map<string, string>([
+  ["&", "&amp;"],
+  ['"', "&quot;"],
+  ["'", "&apos;"],
+  ["<", "&lt;"],
+  [">", "&gt;"],
+]);
+
+function replacer(ch: string): string {
+  return lookup.get(ch)!;
+}
+
+export function html(strings: TemplateStringsArray, ...args: string[]) {
+  let transformed = "";
+  for (let i = 0; i < strings.length; i++) {
+    const arg = args[i];
+    if (arg) {
+      transformed += strings[i] + arg.replace(/[&"'<>]/g, replacer);
+    } else {
+      transformed += strings[i];
+    }
+  }
+  return transformed;
+}
